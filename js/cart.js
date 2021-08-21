2d0e614cf6f7a922d3412c7e153833bbadb537d7
Md.  Raihan Hossain Jibon <53583320+rjibon49@users.noreply.github.com>
2021-08-21 18:17:59 +0600

    update new

--- js/cart.js
+++ js/cart.js
@@ -1,24 +1,28 @@
-function updateProductSelect( price, memorySelect) {
+function updateProductSelect( memorySelect) {
     const memoryInput = document.getElementById('memory-input');
     const memoryNumber = memoryInput.value;
     
     if (memorySelect == true) {
     memoryInput.value = parseInt(memoryNumber) + 180;
-    console.log(memoryInput.value);
+
 } else{
     memoryInput.value = parseInt(memoryNumber);
-    console.log(memoryInput.value);
+
 }
 }
 
-// Memory Price Button
+// Memory Button
 
 document.getElementById('16gb-memory-button').addEventListener('click', function(){
-    updateProductSelect( 180,true);
+    updateProductSelect(true);
+    const memoryCost = document.getElementById('memory-cost');
+    const inputText = memoryCost.innerText;
+    const extraCost = parseFloat(inputText);
+    memoryCost.innerText = extraCost + updateProductSelect();
 })
 
 document.getElementById('8gb-memory-button').addEventListener('click', function(){
-    updateProductSelect(0, false);
+    updateProductSelect(false);
 })
 
 // SSD Storage Button
@@ -26,8 +30,7 @@ document.getElementById('8gb-memory-button').addEventListener('click', function(
 document.getElementById('256gb-ssd-button').addEventListener('click', function(){
     const storageInput = document.getElementById('ssd-input');
     const storageNumber = storageInput.value;
-    // storageInput.value = parseInt(storageNumber);
-    console.log(storageInput.value);
+    storageInput.value = parseInt(storageNumber);
 
 })
 document.getElementById('512gb-ssd-button').addEventListener('click', function(){
@@ -35,35 +38,25 @@ document.getElementById('512gb-ssd-button').addEventListener('click', function()
     const storageInput = document.getElementById('ssd-input');
     const storageNumber = storageInput.value;
     storageInput.value = parseInt(storageNumber) + 100;
-    console.log(storageInput.value);
 })
 document.getElementById('1tb-ssd-button').addEventListener('click', function(){
     // updateProductSelect();
     const storageInput = document.getElementById('ssd-input');
     const storageNumber = storageInput.value;
     storageInput.value = parseInt(storageNumber) + 180;
-    console.log(storageInput.value);
 })
 
 document.getElementById('delivery-free-button').addEventListener('click',function () {
     const deliveryInput = document.getElementById('delivery-input');
     const deliveryNumber = deliveryInput.value;
     deliveryInput.value = parseInt(deliveryNumber);
-    console.log(deliveryInput.value);
 })
+
+// Delivery Cost Button
 
 document.getElementById('delivery-charge-button').addEventListener('click',function () {
     const deliveryInput = document.getElementById('delivery-input');
     const deliveryNumber = deliveryInput.value;
     deliveryInput.value = parseInt(deliveryNumber) + 20;
-    console.log(deliveryInput.value);
 })
 
-
-
-// if (memorySelect == true, memoryNumber<1) {
-//     memoryInput.value = parseInt(memoryNumber) + 20;
-//     console.log(memoryInput.value);
-// } else if(memoryNumber>1){
-//     memoryInput.value = parseInt(memoryNumber);
-// }console.log(memoryInput.value);
